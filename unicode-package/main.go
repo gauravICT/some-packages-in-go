f75844571f660b19d24724ab3944879c0ac14046
gauravICT <44467412+gauravICT@users.noreply.github.com>
2021-05-08 22:57:33 +0530

    Create main.go
    The package unicode has some useful functions for testing characters.
    you can test code here :- https://play.golang.org/p/uiz_brlKTpz

---
+++ unicode-package/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"unicode"
+	"fmt"
+)
+
+func main() {
+	ch := '\u0041'
+	fmt.Println(unicode.IsLetter(ch)) //testing for a letter
+	ch1 := '1'
+	fmt.Println(unicode.IsDigit(ch1)) //testing for a digit
+}
